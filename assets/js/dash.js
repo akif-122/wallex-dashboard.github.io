3bb053935c82ebe1d3f3917f8f3be2abc9251abf
akif-122 <74589084+akif-122@users.noreply.github.com>
2022-08-26 15:45:59 +0500

    wallex dashoboard

---
+++ assets/js/dash.js
@@ -0,0 +1,8 @@
+
+ let mOpener = document.querySelector(".m-menu-opener");
+ let mMenu = document.querySelector(".mobile-menu");
+
+ 	mOpener.addEventListener("click", function(){
+ 		mMenu.classList.toggle('active');
+
+ 	})
